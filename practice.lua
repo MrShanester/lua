fd95d9ebcd7ea0283e931ceb6174b92860c8c38f
Shane Nicholson <shanescottnicholson@gmail.com>
2022-04-09 22:38:03 -0700

    Adds fizzbuzz function to lua

--- practice.lua
+++ practice.lua
@@ -6,4 +6,20 @@ shane = {
   age = 21
 }
 
-print(shane.hair)
+-- print(shane.hair)
+
+--FIZZBUZZ 
+
+function fizz(input)
+  if input % 15 == 0 then
+    return "FIZZBUZZ"
+  elseif input % 5 == 0 then
+    return "FIZZ"
+  elseif input % 3 == 0 then
+    return "BUZZ"
+  else 
+    return "NO BUZZ 4 U"
+  end
+end
+
+print(fizz(8))
